e981ded0150db1920c6bb244fe133c41da63d9eb
Nico Siahaan <nikodemus.jik@gmail.com>
2019-12-02 21:28:21 +0700

    CV

---
+++ func.js
@@ -0,0 +1,10 @@
+
+            function myfunction() {
+                var x = document.getElementById("bot-menu");
+                if(x.style.display == "block"){
+                    x.style.display = "none";
+                }
+                else{
+                    x.style.display = "block";
+                }
+            }
